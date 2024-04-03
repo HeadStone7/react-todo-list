552c9b4a19d406f009fb559fbe839e282f88738f
Josh <jos77marrion@gmail.com>
2024-04-03 09:54:11 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import TotoForm from "./components/toto-form";
+import TodoList from "./components/todo-list";
+import './style/style.css'
+
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (text) => {
+    const todo = {
+      id: Date.now(),
+      text
+    }
+    setTodos([...todos, todo])
+  }
+  const editTodo = (id, newText) => {
+    setTodos(todos.map(todo => todo.id === id ? {...todos, text: newText} : todo))
+  }
+  const removeTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <TotoForm addTodo={addTodo}/>
+        <TodoList todos={todos} editTodo={editTodo} removeTodo={removeTodo}/>
+      </div>
   );
 }
 
